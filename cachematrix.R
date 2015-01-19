38d95f76ea375bfff347422d79f2c759434d97ab
Navid Mahlouji <nmahlouji@gmail.com>
2015-01-19 10:18:24 -0500

    Version 1 no check for identicall matrice

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of writing the following functions is to cache costly
+## calculations to save time. In these functions we try to define
+## a matrix and cache its inverse for further use.
 
-## Write a short comment describing this function
+## The following function is the main function for this purpose
+# it gets the matrix object and provide subset functions to set, get
+# both matrix and its inverse. The variable "inverse" in this function
+# is a global variable which keeps the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(i) inverse <<- i
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## This function is a wraper for "makeCacheMatrix"
+ # which checks and returns the inverse matrix if existed or
+ # calculates it if it not.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if (!is.null(inverse)){
+        print("getting cached data")
+        return(inverse)}
+        data <- x$get()
+        inverse<- solve(data)
+        x$setinverse(inverse)
+        inverse
+    
 }
